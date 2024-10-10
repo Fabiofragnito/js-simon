fc8ba8341b412b6bd6a19372211e5f08fcb9ad6c
Fabio Fragnito <fragnitofabio@gmail.com>
2024-10-10 15:03:51 +0200

    aggiunto metodo joint

--- main.js
+++ main.js
@@ -5,15 +5,27 @@
 
 
 
-    let numbers = [];  /* creo un array di 5 random numbers */ 
+    // let numbers = [];  /* creo un array di 5 random numbers */ 
 
-    for (let i = 0; i < 5; i++) { /* faccio un ciclo per iterare 5 numeri e li pusho nella array nembers */
-        numbers.push(Math.floor(Math.random()*100)) +1;        
+    // for (let i = 0; i < 5; i++) { /* faccio un ciclo per iterare 5 numeri e li pusho nella array nembers */
+    //     numbers.push(Math.floor(Math.random()*100)+1) ;        
+    // }
+
+    // const numberContainer = document.getElementById("randomNumbers") /* creo un container dove visualizzare i numeri */
+    // const p = document.createElement('p') /* creo un elemeto p */
+    // numberContainer.appendChild(p); /* aggiungo il p al div */
+
+
+    function generateRandomNumber() {
+        let numbers = [];
+        for (let i = 0; i < 5; i++) { /* faccio un ciclo per iterare 5 numeri e li pusho nella array nembers */
+            numbers.push(Math.floor(Math.random()*100)) +1;        
+        }
+        const numberContainer = document.getElementById("randomNumbers") /* creo un container dove visualizzare i numeri */
+        const p = document.createElement('p') /* creo un elemeto p */
+        p.textContent = numbers.join(',') /* aggiungo i numeri al p */
+        numberContainer.appendChild(p); 
+        
     }
 
-    const numberContainer = document.getElementById("randomNumbers") /* creo un container dove visualizzare i numeri */
-    const p = document.createElement('p') /* creo un elemeto p */
-    numberContainer.appendChild(p); /* aggiungo il p al div */
-
-
-    
+    console.log(numbers);
