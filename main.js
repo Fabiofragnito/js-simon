78909282f49f67de6e5c8e1d95758a568ff3ff5e
Fabio Fragnito <fragnitofabio@gmail.com>
2024-10-10 14:55:46 +0200

    creato container e p

--- main.js
+++ main.js
@@ -8,6 +8,12 @@
     let numbers = [];  /* creo un array di 5 random numbers */ 
 
     for (let i = 0; i < 5; i++) { /* faccio un ciclo per iterare 5 numeri e li pusho nella array nembers */
-        numbers.push(Math.floor(Math.random()*100)) +1;       
-        
+        numbers.push(Math.floor(Math.random()*100)) +1;        
     }
+
+    const numberContainer = document.getElementById("randomNumbers") /* creo un container dove visualizzare i numeri */
+    const p = document.createElement('p') /* creo un elemeto p */
+    numberContainer.appendChild(p); /* aggiungo il p al div */
+
+
+    
